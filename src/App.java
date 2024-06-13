c156b87f3907c6ebb9ab7ad257b98ca2ac01cb54
Danishwer _ <dannywits12321@gmail.com>
2024-06-13 16:08:01 +0530

    git

--- src/App.java
+++ src/App.java
@@ -3,9 +3,9 @@ import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Connection connection = DataBase.setConnection();
-        DataBase.getDetails(connection);
-        connection.close();
+        //Connection connection = DataBase.setConnection();
+       // DataBase.getDetails(connection);
+        //connection.close();
         new Login();
     }
 }
