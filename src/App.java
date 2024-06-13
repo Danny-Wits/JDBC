d54e2efab8b230f74cadb7f5f8eeaec51ccd039a
Danishwer _ <dannywits12321@gmail.com>
2024-06-13 16:39:38 +0530

    git

--- src/App.java
+++ src/App.java
@@ -5,6 +5,6 @@ public class App {
        // DataBase.getDetails(connection);
         //connection.close();
         new Login();
-        System.err.println("...");
+        System.err.println("....");
     }
 }
