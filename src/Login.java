a907f0e261870c5481ccdd369434775dd14d0863
Danishwer _ <dannywits12321@gmail.com>
2024-06-13 16:10:55 +0530

    git

--- src/Login.java
+++ src/Login.java
@@ -1,16 +1,14 @@
-import java.awt.Window;
-
 import javax.swing.JFrame;
 
 public class Login extends JFrame {
 
     public Login() {
         setup();
-        
+
     }
     private void setup(){
         setTitle("Login Page");
-        setBounds(0, 0, Window.WIDTH, Window.HEIGHT);
+        setBounds(0, 0,500 ,500 );
         setVisible(true);
     }
 
