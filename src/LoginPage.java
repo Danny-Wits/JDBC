3f51caea5337a7265b287df9ed7804cf96833865
Danishwer _ <dannywits12321@gmail.com>
2024-06-13 23:07:23 +0530

    inputs added

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,14 +1,37 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 public class LoginPage extends JFrame{
+    private JTextField nameI,passwordI;
     private void setUp(){
        setTitle("LoginPage");
        setBounds(100,100,500,700);
        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       setVisible(true); 
     }
+    
     LoginPage(){
         setUp();
+        JPanel center = new JPanel();
+        center.setLayout(new GridLayout(2,2));
+        //Name 
+        JLabel nameL=new JLabel("NAME : ");
+        nameI=new JTextField();
+
+        //Name 
+        JLabel passwordL=new JLabel("PASSWORD : ");
+        passwordI=new JTextField();
+        
+        center.add(nameL);
+        center.add(nameI);
+        center.add(passwordL);
+        center.add(passwordI);
+        
+        add(center,BorderLayout.CENTER);
+        setVisible(true);
     }
     
 }
